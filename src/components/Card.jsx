a55c234cc5b3bd76de2980f3a3fc9e0adbf4c856
Shivanshu Kumar Maurya <shivashchandramaurya@gmail.com>
2024-04-10 11:36:56 +0530

    Updated Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,21 +5,26 @@ import ProgressBar from "@ramonak/react-progress-bar";
 import { AiOutlineClockCircle } from "react-icons/ai";
 
 const Card = (props) => {
+  const truncateText = ((text, maxLen) => {
+    if(text.length === '') return text;
+    if(text.length <= maxLen) return text;
+    return text.slice(0, maxLen) + '...';
+  });
   return (
-    <div className=' w-[90%] rounded-md h-auto bg-gray-200 text-gray-600 hover:shadow-primary hover:shadow-xl hover:-translate-y-4 hover:transition-all hover:duration-500'>
+    <div className=' w-[90%] rounded-md h-[550px] bg-gray-200 text-gray-600 hover:shadow-primary hover:shadow-xl hover:-translate-y-4 hover:transition-all hover:duration-500'>
       <div style={{ width: "100%", height: "100", background: "#OOFFFFFF" }}>
         <Image
           src={"/gareeb.png"}
           alt=""
           width="1"
-          height="1"
+          height="1"  
           layout="responsive"
           objectFit="contain"
         />
       </div>
       <div className=' px-5'>
-        <h1 className=' text-xl font-bold text-center mb-3'>{props.name}</h1>
-        <p className=' font-normal text-md mb-8'>{props.desc}</p>
+        <h1 className=' text-xl font-bold text-center mb-3'>{truncateText(props.name, 20)}</h1>
+        <p className=' font-normal text-md mb-8'>{truncateText(props.desc, 100)}</p>
         <ProgressBar completed={60} bgColor='#3437eb' animateOnRender={true} baseBgColor='#ffffff' borderRadius='5px' />
         <div className='flex justify-end gap-1 mt-4'>
           <AiOutlineClockCircle />
